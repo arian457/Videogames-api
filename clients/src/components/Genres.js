9e0ba689ba5f2ab3a03ec1561df8a10e048a0347
Arian <82475239+arian457@users.noreply.github.com>
2021-08-12 14:35:04 -0300

    improved Genres filter

--- clients/src/components/Genres.js
+++ clients/src/components/Genres.js
@@ -1,34 +1,32 @@
 import {connect} from 'react-redux'
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import { useRouteMatch } from 'react-router'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { filterByGenre } from '../redux/actions'
 import '../styles/Genres.css'
-  function Genres ({games, genres}){
+import { PageButtons } from './PageButtons'
+  function Genres ({games, genres, filterByGenre}){
      const match = useRouteMatch()
      const id = parseInt(match.params.genresId)
-     genres = genres.filter(gen => gen.id != id)
-     games = games.filter(game => {
-      if(game.genres) {  
-     let flag = game.genres.find(gen => gen.id === id)
-         if (flag) return game
-        }
-     })
+      useEffect(() => {
+         filterByGenre(id)
+     }, [id])
     
    return(<div>
        <h2>Otros generos:</h2>
-       <div className = 'genres-div'>
+       <div className='genres-div'>
        {genres.map(gen => {
            return(
-               <Link to ={`/genres/${gen.id}`}> {gen.name} </Link>
+               <Link to ={`/Home/genres/${gen.id}`}> {gen.name} </Link>
            )})}
-           </div>
+       </div>
            <div className = 'cards-div' id='cards-div-genres-display'>
         { games.map(game => {
           return (<div className= 'cards-item' key={game.id}>
                   <img src={game.img} />
                   <Link to = {`/game/${game.id}`}><h2>{game.name}</h2></Link> 
                   <span>{game.genres.map(gen => {
-                      return(<strong><Link to={`/genres/${gen.id}`}> {gen.name} </Link> </strong>)
+                      return(<strong><Link to={`/Home/genres/${gen.id}`}> {gen.name} </Link> </strong>)
                     })}
                     </span>
                     </div>)}) }
@@ -39,9 +37,12 @@ import '../styles/Genres.css'
  
  const mapStateToProps = (state) => {
      return{
-         games : state.allGames,
+         games : state.shownGames,
          genres: state.genres
      }
- }
+}
+const mapDispatchToProps = {
+    filterByGenre: id => filterByGenre(id)
+}
 
- export default connect(mapStateToProps, null)(Genres)
+ export default connect(mapStateToProps, mapDispatchToProps)(Genres)
