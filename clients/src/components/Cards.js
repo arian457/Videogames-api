f608865db2322629567d7dcf048c471ad3195268
Arian <82475239+arian457@users.noreply.github.com>
2021-08-12 14:36:43 -0300

    improved COMPONENT

--- clients/src/components/Cards.js
+++ clients/src/components/Cards.js
@@ -1,61 +1,55 @@
-import {connect} from 'react-redux'
-import {togglePage} from '../redux/actions'
-import {useState, useEffect} from 'react'
+import { connect } from 'react-redux'
+import { togglePage } from '../redux/actions'
+import { useState, useEffect } from 'react'
 import '../styles/Cards.css'
 import PacmanLoader from "react-spinners/PacmanLoader";
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import PageButtons from './PageButtons';
 
-const Cards = ({games,loader,togglePage}) => {
+const Cards = ({ games, loader}) => {
+ 
   const [loading, toggleLoading] = useState(true)
   useEffect(() => {
-    if(!loader){
+    if (!loader) {
       toggleLoading(false)
     }
-  },[loader])
-  
+  }, [loader])
+
   return (<div className='cards'>
-       {!loading ?  <div className='btn-group'>
-       <button className='change-page' onClick={() => 
-          togglePage("-")
-          } >←</button>
-        <button className='change-page' onClick={() => 
-          togglePage("+")
-          } >→</button> 
-           </div>
-           : null}
-       <div className= 'cards-div'>
+   
+    <div className='cards-div'>
       {games.length > 0 ? games.map(game => {
-          return (<div className= 'cards-item' key={game.id}>
-                  <img src={game.img} />
-                 <Link to = {`/game/${game.id}`}><h2>{game.name}</h2></Link> 
-                 {game.genres ? <span>{game.genres.map(gen => {
-                      return(<strong><Link to={`/genres/${gen.id}`}> {gen.name} </Link> </strong>)
-                    })}
-                    </span>:<span>sin generos</span>}
-                    
-               </div>)
+        return (<div className='cards-item' key={game.id}>
+          <img src={game.img} />
+          <Link to={`/game/${game.id}`}><h2>{game.name}</h2></Link>
+          {game.genres ? <span>{game.genres.map(gen => {
+            return (<strong><Link to={`/Home/genres/${gen.id}`}> {gen.name} </Link> </strong>)
+          })}
+          </span> : <span>sin generos</span>}
+
+        </div>)
       }) : <PacmanLoader size={100} loading={loading} />}
-      </div>
-     
-       </div>)
+    </div>
+
+  </div>)
 
 }
 
 
 function mapStateToProps(state) {
-    return {
-      games: state.shownGames,
-      page: state.pageIndex,
-      loader: state.loading
-    };
+  return {
+    games: state.shownGames,
+    loader: state.loading,
+
+
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+
+    togglePage: (index) => dispatch(togglePage(index))
   }
-  
-  function mapDispatchToProps (dispatch) {
-    return {
-      
-      togglePage : (index) => dispatch(togglePage(index)) 
-    }
-  }
-  
-  export default connect(mapStateToProps, mapDispatchToProps)(Cards);
-  
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Cards);
